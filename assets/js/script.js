fbd56ba606d7820c6d90db9f11ae56eae57f2719
Szeccsa <95046202+Szeccsa@users.noreply.github.com>
2024-10-23 15:32:25 +0200

    Update

---
+++ assets/js/script.js
@@ -0,0 +1,97 @@
+// Debugging stuff
+$(document).ready(function() {
+    if (typeof jQuery !== 'undefined') {
+        console.log("jQuery is loaded.");
+        console.log("jQuery version:", jQuery.fn.jquery);
+        if (jQuery.noConflict) {
+            console.log("jQuery is in noConflict mode.");
+        }
+    } else {
+        console.log("jQuery is not loaded.");
+    }
+});
+
+// Jquery Include HTML
+$(document).ready(function () {
+    function includeHTML() {
+        $('[include-html]').each(function () {
+            var $this = $(this);
+            var file = $this.attr('include-html');
+            if (file) {
+                $.ajax({
+                    url: file,
+                    method: 'GET',
+                    success: function (data) {
+                        $this.html(data);
+                        console.log('Successfully included:', file);
+                    },
+                    error: function () {
+                        $this.html('An error has occurred while loading this section.');
+                        console.error('Failed to include:', file);
+                    },
+                    complete: function () {
+                        $this.removeAttr('include-html');
+                        includeHTML();
+                    }
+                });
+            }
+        });
+    }
+
+    includeHTML();
+});
+
+// Jquery dropdown menu with slide effect
+$(document).ready(function () {
+    $('.hamburger').click(function () {
+        $(this).toggleClass('active');
+        $('.dropdown').slideToggle(300);
+    });
+});
+
+// Function to handle screen width changes
+function checkScreenWidth() {
+    console.log("CheckScreenWidth");
+}
+
+window.addEventListener('resize', checkScreenWidth);
+
+// Script to display device details
+$(document).ready(function () {
+    function getBrowserAndDeviceDetails() {
+        var userAgent = navigator.userAgent;
+        var browserName, deviceType;
+
+        // Detect browser
+        if (userAgent.indexOf("Firefox") > -1) {
+            browserName = "Mozilla Firefox";
+        } else if (userAgent.indexOf("SamsungBrowser") > -1) {
+            browserName = "Samsung Internet";
+        } else if (userAgent.indexOf("Opera") > -1 || userAgent.indexOf("OPR") > -1) {
+            browserName = "Opera";
+        } else if (userAgent.indexOf("Trident") > -1) {
+            browserName = "Microsoft Internet Explorer";
+        } else if (userAgent.indexOf("Edge") > -1) {
+            browserName = "Microsoft Edge";
+        } else if (userAgent.indexOf("Chrome") > -1) {
+            browserName = "Google Chrome";
+        } else if (userAgent.indexOf("Safari") > -1) {
+            browserName = "Apple Safari";
+        } else {
+            browserName = "Unknown Browser";
+        }
+
+        // Detect device
+        if (/Mobi|Android/i.test(userAgent)) {
+            deviceType = "Mobile";
+        } else if (/Tablet|iPad/i.test(userAgent)) {
+            deviceType = "Tablet";
+        } else {
+            deviceType = "Desktop";
+        }
+
+        return `You are using ${browserName} on a ${deviceType} device.`;
+    }
+    
+    $("#browserDetails").text(getBrowserAndDeviceDetails());
+});
